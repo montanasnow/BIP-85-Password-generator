e75d20c5524c2b0f1a23db798b4284752564f57f
Jack Connors <montanasnow@gmail.com>
2022-10-07 21:27:10 -0600

    Ver 1.0

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
-// use base64::encode;
-use bitcoin::hashes::{hmac, sha512, HashEngine};
-use bitcoin::secp256k1::{self, Secp256k1};
-// use bitcoin::secp256k1::{Secp256k1, SecretKey};
-
-// use bitcoin::hashes::{hmac, sha512, HashEngine};
-// use bitcoin::bitcoin_hashes::Hash;
+use bip32::{Mnemonic, Prefix, XPrv};
 use bitcoin::hashes::Hash;
+use bitcoin::hashes::{hmac, sha512, HashEngine};
 use bitcoin::util::bip32::ChildNumber;
 use bitcoin::util::bip32::DerivationPath;
 use bitcoin::util::bip32::ExtendedPrivKey;
 use clap::Parser;
-// use hmac_sha512::HMAC;
+use rand_core::OsRng;
 use std::str::FromStr;
 
 #[derive(Parser)]
@@ -18,7 +13,6 @@ use std::str::FromStr;
 
 // #[clap(about, version, author)]
 struct Args {
-    // #[clap(long)]
     xprv: String,
 
     #[clap(short = 'L', long, default_value = "21")]
@@ -32,21 +26,39 @@ fn main() {
     let args = Args::parse();
 
     println!("\n");
-    println!("    ________________________            ______   ________    __________                                                ___                                               __                ");
-    println!("    \\______   \\   \\______   \\          /  __  \\ |   ____/    \\______   \\_____    ______ ________  _  _____________  __| _/       ____   ____   ____   ________________ _/  |_  ___________ ");
-    println!("     |    |  _/   ||     ___/  ______  >      < |____  \\      |     ___/\\__  \\  /  ___//  ___/\\ \\/ \\/ /  _ \\_  __ \\/ __ |       / ___\\_/ __ \\ /    \\_/ __ \\_  __ \\__  \\\\   __\\/  _ \\_  __ \\");
-    println!("     |    |   \\   ||    |     /_____/ /   --   \\/       \\     |    |     / __ \\_\\___ \\ \\___ \\  \\     (  <_> )  | \\/ /_/ |      / /_/  >  ___/|   |  \\  ___/|  | \\// __ \\|  | (  <_> )  | \\/");
-    println!("     |______  /___||____|             \\______  /______  /     |____|    (____  /____  >____  >  \\/\\_/ \\____/|__|  \\____ |      \\___  / \\___  >___|  /\\___  >__|  (____  /__|  \\____/|__|   ");
-    println!("            \\/                               \\/       \\/                     \\/     \\/     \\/                          \\/     /_____/      \\/     \\/     \\/           \\/                   ");
+    println!("    ________________________            ______   ________ __________                                                ___   ________                                   __                ");
+    println!("    \\______   \\   \\______   \\          /  __  \\ |   ____/ \\______   \\_____    ______ ________  _  _____________  __| _/  /  _____/  ____   ____   ________________ _/  |_  ___________ ");
+    println!("     |    |  _/   ||     ___/  ______  >      < |____  \\   |     ___/\\__  \\  /  ___//  ___/\\ \\/ \\/ /  _ \\_  __ \\/ __ |  /   \\  ____/ __ \\ /    \\_/ __ \\_  __ \\__  \\\\   __\\/  _ \\_  __ \\");
+    println!("     |    |   \\   ||    |     /_____/ /   --   \\/       \\  |    |     / __ \\_\\___ \\ \\___ \\  \\     (  <_> )  | \\/ /_/ |  \\    \\_\\  \\  ___/|   |  \\  ___/|  | \\// __ \\|  | (  <_> )  | \\/");
+    println!("     |______  /___||____|             \\________/________/  |____|    (____  /____  >____  >  \\/\\_/ \\____/|__|  \\____ |   \\______  /\\___  >___|  /\\___  >__|  (____  /__|  \\____/|__|   ");
+    println!("            \\/                                                            \\/     \\/     \\/                          \\/          \\/     \\/     \\/     \\/           \\/                   ");
+    println!("\n");
+    println!("     https://github.com/montanasnow/BIP-85-Password-generator");
+    println!("     Derives a deterministic password from the key nor mnemonic based on BIP-85");
+    println!("     Please provide a BIP-32 root key (xprv...) or a BIP-39 mnemonic.");
+    println!("     Options: -L for password length (default 21) and -I index (default 0)");
+    println!("     Based on https://coldcard.com/docs/bip85-passwords and https://github.com/scgbckbone/bips/blob/passwords/bip-0085.mediawiki#PWD and https://github.com/scgbckbone/btc-hd-wallet/blob/master/tests/test_bip85.py");
+    println!("     Use as your own risk. AS-IS! Not for use in Europe.");
     println!("\n");
 
-    println!("    XPRV:   {}", args.xprv);
-    println!("    Length: {:?}", args.pwd_len);
-    println!("    Index:  {:?}", args.index);
+    let root_xprv = if !args.xprv.is_empty() && args.xprv.starts_with("xprv") {
+        println!("     XPRV:            {}", &args.xprv);
+        ExtendedPrivKey::from_str(&*args.xprv).unwrap()
+    } else if !args.xprv.is_empty() {
+        println!("     Mnemonic:        {}", &args.xprv);
+        let mnemonic = Mnemonic::new(&args.xprv, Default::default()).unwrap();
+        let seed = mnemonic.to_seed("");
+        ExtendedPrivKey::from_str(&XPrv::new(&seed).unwrap().to_string(Prefix::XPRV)).unwrap()
+    } else {
+        let mnemonic = Mnemonic::random(&mut OsRng, Default::default());
+        let seed = mnemonic.to_seed("");
+        ExtendedPrivKey::from_str(&XPrv::new(&seed).unwrap().to_string(Prefix::XPRV)).unwrap()
+    };
 
-    let secp = Secp256k1::new();
-    let root = ExtendedPrivKey::from_str(&*args.xprv).unwrap();
-    // println!("EX {:?}", root.private_key.to_string());
+    println!("     Password Length: {:?}", args.pwd_len);
+    println!("     Index:           {:?}", args.index);
+
+    let secp = bitcoin::secp256k1::Secp256k1::new();
 
     let path = DerivationPath::from(vec![
         ChildNumber::Hardened { index: 707764 },
@@ -54,29 +66,17 @@ fn main() {
         ChildNumber::from_hardened_idx(args.index).unwrap(),
     ]);
 
-    let data = derive(&secp, &root, &path);
-
-    let entropyb = base64::encode(&data[0..64]);
-    let password = entropyb[0..args.pwd_len as usize].to_string();
-    println!("\n");
-    println!("Password: {}", &password);
-    println!("\n");
-}
-
-fn derive<C: secp256k1::Signing, P: AsRef<[ChildNumber]>>(
-    secp: &Secp256k1<C>,
-    root: &ExtendedPrivKey,
-    path: &P,
-) -> Vec<u8> {
-    //     if index >= 0x80000000 {
-    //         return Err(Error::InvalidIndex(index));
-    //     }
-
     const BIP85_CHILD_NUMBER: ChildNumber = ChildNumber::Hardened { index: 83696968 };
-    let bip85_root = root.ckd_priv(&secp, BIP85_CHILD_NUMBER).unwrap();
+    let bip85_root = root_xprv.ckd_priv(&secp, BIP85_CHILD_NUMBER).unwrap();
     let derived = bip85_root.derive_priv(&secp, &path).unwrap();
     let mut h = hmac::HmacEngine::<sha512::Hash>::new("bip-entropy-from-k".as_bytes());
     h.input(&derived.private_key.to_bytes());
-    let data = hmac::Hmac::from_engine(h).into_inner();
-    data.to_vec()
+    let data = hmac::Hmac::from_engine(h).into_inner().to_vec();
+
+    let entropy_b64 = base64::encode(&data[0..64]);
+    let password = entropy_b64[0..args.pwd_len as usize].to_string();
+
+    println!("\n");
+    println!("Password: {}", &password);
+    println!("\n");
 }
